9b68840bfd484f17934e26c32392130850cc9c1a
Skobeltsyn <konstantin@skobeltsyn.com>
2020-10-23 17:07:27 +0300

    ee

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import sys  # sys нужен для передачи argv в QApplication
-from PyQt5 import QtWidgets
+import sys
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication
+
+
 
 class YandexApp(QtWidgets.QMainWindow):
+
     def __init__(self):
         super().__init__()
-        self.setup_ui()  # Это нужно для инициализации нашего дизайна
+        uic.loadUi("design.ui", self)
+        self.setup_ui()
+
     def setup_ui(self):
         pass
 
 def main(name):
     app = QtWidgets.QApplication(sys.argv)
-    window = YandexApp()  # Создаём объект класса ExampleApp
-    window.show()  # Показываем окно
-    app.exec_()  # и запускаем приложение
+    window = YandexApp()
+    window.show()
+    app.exec_()
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main('PyCharm')
-# eg
