7a5622c38f3706926eb8b11b1a29547f6437d6ae
Skobeltsyn <konstantin@skobeltsyn.com>
2020-10-24 18:26:38 +0300

    ee

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
+import csv
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QFileDialog
 
 
 class YandexApp(QtWidgets.QMainWindow):
-
     def __init__(self):
         self.my_csv_path = ""
         super().__init__()
@@ -16,6 +16,8 @@ class YandexApp(QtWidgets.QMainWindow):
         pass
 
     def trigger_stage_1(self):
+        self.content_1.setVisible(True)
+        self.content_2.setVisible(False)
         self.openFileBtn.clicked.connect(self.get_csv)
         self.openFileBtn.setStyleSheet("background: #c3fb12; font-size: 20px; padding: 9px; border-radius: 10px;")
         self.progressBar.setValue(0)
@@ -39,11 +41,15 @@ class YandexApp(QtWidgets.QMainWindow):
     def get_csv(self):
         self.my_csv_path = QFileDialog.getOpenFileName(self, 'Выбрать файл с данными', '')[0]
         self.csv_file_path.setText(self.my_csv_path)
-        self.chooseDataRows.setStyleSheet("background: #c3fb12; font-size: 20px; padding: 9px; border-radius: 10px; color: #000000;")
+        self.chooseDataRows.setStyleSheet("background: #c3fb12; font-size: 20px; padding: 9px; border-radius: 10px; "
+                                          "color: #000000;")
         self.chooseDataRows.setEnabled(True)
         self.openFileBtn.setStyleSheet("background: transparent; color: #c3fb12; border: 1px solid #c3fb12; "
                                        "font-size: 20px; padding: 9px; border-radius: 10px;")
         self.chooseDataRows.clicked.connect(self.trigger_stage_2)
+
+    # def open_csv(self):
+
 
 def main(name):
     app = QtWidgets.QApplication(sys.argv)
